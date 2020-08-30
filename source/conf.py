4824381c5e0851f60ac3e2eda1807517a8687fe3
Jani Tiainen <jani@tiainen.cc>
2020-08-30 22:07:27 +0300

    Version bump

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019-2020, Jani Tiainen'
 author = 'Jani Tiainen'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.0.4'
 
 
 # -- General configuration ---------------------------------------------------
